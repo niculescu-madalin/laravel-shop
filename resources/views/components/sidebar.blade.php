5c0e8edc787c6157de8db55a371ed27e2019fac6
niculescu-madalin <116899982+niculescu-madalin@users.noreply.github.com>
2025-04-24 16:50:37 +0300

    Changed the way hidding the sidebar works, being available only on small devices

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -26,13 +26,14 @@
 
     <button  
         @click="open = !open" 
-        class="text-white mb-4 px-4 py-2 bg-gray-700 rounded hover:bg-gray-600" :class="{ 'w-full': open, 'w-fit': !open }"
+        class="text-white mb-4 px-4 py-2 bg-gray-700 rounded hover:bg-gray-600 block sm:hidden"
     >
-        <span x-show="open">Hide Sidebar</span>
-        <span x-show="!open" >
+        <span x-show="open">Hide filtering options</span>
+        <span x-show="!open" class="flex items-center gap-2 w-full justify-center">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z" />
             </svg>  
+            Filtering Options
         </span>
     </button>
 
